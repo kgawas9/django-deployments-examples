d27829ff945fb969d9084f4c95dfa0800e519b59
Kiran <kgawas9@gmail.com>
2020-12-31 18:36:11 +0530

    'first'

---
+++ login_project/login_app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from login_app import views
+
+app_name = 'login_app'
+
+urlpatterns = [
+    url(r'^register',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+]
